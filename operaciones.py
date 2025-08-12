e4841750e4b81c4898458485c35ad6df7f9eb663
Lilliana Uribe <86997181+LillianaU@users.noreply.github.com>
2025-08-12 08:56:52 -0600

    operaciones
    operaciones

---
+++ operaciones.py
@@ -0,0 +1,20 @@
+#suma de vectores
+import numpy as np
+
+vector1 = np.array([1, 2, 3])
+vector2 = np.array([4, 5, 6])
+suma = vector1 + vector2 # Suma de vectores
+print("Suma de vectores:", suma)  # Imprimimos la suma de los vectores
+#multiplicacion de vectores
+producto = vector1 * vector2 # Multiplicación de vectores
+print("Producto de vectores:", producto)  # Imprimimos el producto de los vectores
+#division de vectores
+division = vector1 / vector2 # División de vectores 
+print("\033[35m División de vectores:", division,"\035[0m")  # Imprimimos la división de los vectores
+#"resta de vectores
+resta = vector1 - vector2 # Resta de vectores
+print("\033[36m Resta de vectores:", resta,"\033[0m")  # Imprimimos la resta de los vectores#
+# potencia de vectores
+potencia = vector1 ** 2 # Potencia de vectores
+
+print("\033[34mResultado potencia:", potencia, "\033[0m")# Imprimimos la potencia de los
